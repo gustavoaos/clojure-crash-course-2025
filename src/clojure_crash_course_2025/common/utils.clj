ea3f0f8080a1dece07fc265209bc35f5921fdd20
gustavoaos <gustavo_alves@outlook.com>
2025-05-31 17:41:50 -0300

    feat: add common utils namespace

---
+++ src/clojure_crash_course_2025/common/utils.clj
@@ -0,0 +1,5 @@
+(ns clojure-crash-course-2025.common.utils)
+
+(defn add
+  [x y]
+  (+ x y))
--- src/clojure_crash_course_2025/core.clj
+++ src/clojure_crash_course_2025/core.clj
@@ -1,6 +1,8 @@
 (ns clojure-crash-course-2025.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure-crash-course-2025.common.utils :as utils]))
 
 (defn -main
   []
-  (println "Clojure Crash Course 2025"))
+  (println "Clojure Crash Course:"
+           (utils/add 2000 25)))
