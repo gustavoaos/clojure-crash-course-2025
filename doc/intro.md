891aa95a197e46b44302b8e42528993212bbbd0a
gustavoaos <gustavo_alves@outlook.com>
2025-05-31 16:45:43 -0300

    chore: inital clojure project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-crash-course-2025
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/clojure_crash_course_2025/core.clj
@@ -0,0 +1,7 @@
+(ns clojure-crash-course-2025.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
