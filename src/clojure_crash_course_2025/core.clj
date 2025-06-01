bca3009c2199949e05ec3e994b88d10fdd1762ad
gustavoaos <gustavo_alves@outlook.com>
2025-05-31 23:33:53 -0300

    feat: functions on clojure.core

--- src/clojure_crash_course_2025/core.clj
+++ src/clojure_crash_course_2025/core.clj
@@ -25,6 +25,41 @@
   (println "Result of adding" arg1 "and" arg2 "equals" result))
 
 ; Data structures
-[1 2 3]                                                     ; list
+[1 2 3]                                                     ; vector
 { "a" [1 2], "b" [3 4], 12 5}                               ; map
 #{1 2 3}                                                    ; set
+
+; Clojure functions
+; fn we are defining here anonymous functions
+(def vector [1 2 3 4 5])
+
+; map
+(map (fn [element]
+       (+ element 1)) vector)
+
+; filter
+(filter (fn [element]
+          (odd? element)) vector)
+
+; remove
+(remove (fn [element]
+          (odd? element)) vector)
+
+; reduce
+(reduce (fn [acc elem]
+          (+ acc elem)) vector)
+
+(reduce (fn [acc elem]
+          (println (+ acc elem)) (+ acc elem)
+          ) vector)
+
+(reduce (fn [acc elem]
+          (let [result (+ acc elem)]
+            (println result) result)
+          ) vector)
+
+; group-by
+(group-by even? vector)                                     ; returns a map
+
+; frequencies
+(frequencies vector)
